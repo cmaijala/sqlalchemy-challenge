b1e77b8a6e202b310ed8234eff255d67cd667967
cchea09-YtskQe <cchea09@gmail.com>
2024-08-29 23:30:02 -0500

    ORM and EPA done

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,9 +1,23 @@
 # Import the dependencies.
-
+from pathlib import Path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+import pandas as pd
 
 
 #################################################
 # Database Setup
+####### Create a reference to the file.
+database_path = Path("../Resources/Census_Data.sqlite")
+
+
+####### Create a connection that can talk to the database
+conn = create_engine(f"sqlite:///{database_path}").connect()
+
+
+#################### see if it works
+data = pd.read_sql("SELECT * FROM icecreamstore", conn)
+data.head()
 #################################################
 
 
